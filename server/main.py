7b5638ce5a13a6353155cbf6862b1d29827afb46
Suraj Pisal <surajpisal113@gmail.com>
2023-04-24 11:08:57 +0530

    removed server

--- server/main.py
+++ server/main.py
@@ -1,13 +1,36 @@
 
-from server.db import client
 import uuid
 import datetime
 import streamlit as st
+import os 
 
 
 def get_user_id():
     user_id = st.secrets["user_id"]
     return user_id
+
+def pdfToText(path):
+    pdfreader = PyPDF2.PdfFileReader(path)
+    no_of_pages = pdfreader.numPages
+    with open('final_txt.txt', 'w') as f:
+        for i in range(0, no_of_pages):
+            pagObj = pdfreader.getPage(i)
+            f.write(pagObj.extractText())
+    with open('final_txt.txt', 'r') as f:
+        text = f.read()
+    if os.path.exists("final_txt.txt"):
+        os.remove("final_txt.txt")
+        return text
+
+def convert_pdf_to_handwriting(file):
+    user_id = get_user_id()
+    file_name = file.name
+    file_type = file.type
+    file_id = str(uuid.uuid4())
+    file_path = f"uploads/{file_id}.{file_type}"
+    with open(file_path, "wb") as f:
+        f.write(file.getbuffer())
+    return file_path
 
 
 st.title("document to handwritting convertor")
@@ -19,5 +42,18 @@ if file is not None:
     # print file details
     st.write("file name: ", file.name)
     st.write("file type: ", file.type)
+
+    # convert file to handwriting
+    convert_button = st.button("convert")
+    if convert_button:
+        file_path = convert_pdf_to_handwriting(file)
+        st.write("file converted successfully")
+        st.write("file path: ", file_path)
+
+        # download converted file
+        download_button = st.button("download")
+        if download_button:
+            st.markdown(f"[download file]({file_path})")
+
 
     
