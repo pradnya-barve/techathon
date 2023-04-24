06924c2b0a478929f2b33fb51e433b1dce414a49
Suraj Pisal <surajpisal113@gmail.com>
2023-04-24 10:30:13 +0530

    added upload file and download file routes

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+from fastapi import File, UploadFile
 
 app = FastAPI()
 
@@ -18,6 +19,16 @@ app.add_middleware(
 @app.get("/")
 async def main():
     return {"message": "Hello World"}
+
+# function to upload file
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
+
+# function to return the converted file
+@app.get("/convertfile/")
+async def convert_file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
 
 
 if __name__ == "__main__":
